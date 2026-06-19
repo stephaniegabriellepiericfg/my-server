8fe3903fb3b443f26779e3a772432907a83847ac
Stephanie Gabrielle Pieri <stephaniegabriellepieri@192.168.1.106>
2026-06-19 16:32:36 +0100

    Updating port

--- server.js
+++ server.js
@@ -16,4 +16,5 @@ app.get('/tasks', (req, res) => {
   res.json(tasks);
 });
 
-app.listen(3001, () => console.log('Server running on port 3001'));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
